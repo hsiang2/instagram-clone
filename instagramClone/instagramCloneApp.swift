f8decef7c700ed3a40110e6e0c081e73ce542bb0
Hsiang <q224399377@gmail.com>
2023-02-05 14:34:24 +0800

    Initial Commit

---
+++ instagramClone/instagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramCloneApp.swift
+//  instagramClone
+//
+//  Created by 蔡相襄 on 2023/2/5.
+//
+
+import SwiftUI
+
+@main
+struct instagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
